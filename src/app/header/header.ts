b2853ea978456c5b14637a7cddce0ebbdafaacbb
Viet Nguyen Huy <viet.nguyenhuy@nashtechglobal.com>
2025-10-30 14:15:30 +0700

    Fix bug show wrong active page in Recipe list

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { NgClass } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [NgClass, RouterLink],
+  imports: [NgClass, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
