322f93bdb388783769659f70bac9c97c0c02a4b0
Viet Nguyen Huy <viet.nguyenhuy@nashtechglobal.com>
2025-10-30 16:50:36 +0700

    Fix bug data is not shown after click Recipe list in menu

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe/recipe.model';
 import { RecipeService } from '../recipe/recipe.service';
 import { RouterLink } from "@angular/router";
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.scss',
 })
 export class RecipeList implements OnInit {
-  recipes: Recipe[] = [];
+  recipes$!: Observable<Recipe[]>;
 
   constructor(private recipeService: RecipeService) {}
   ngOnInit(): void {
-    this.recipes = this.recipeService.getRecipes();
+    this.recipes$ = this.recipeService.getRecipes();
   }
 }
