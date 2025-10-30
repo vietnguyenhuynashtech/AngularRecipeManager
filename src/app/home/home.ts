0e43eb089cdf09860d3e400b6d981002c1299f4c
Viet Nguyen Huy <viet.nguyenhuy@nashtechglobal.com>
2025-10-30 14:12:29 +0700

    Add content to Home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
