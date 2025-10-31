6beddbcad04837178bdc0285c643c057912f781b
Viet Nguyen Huy <viet.nguyenhuy@nashtechglobal.com>
2025-10-31 09:34:28 +0700

    Fix bug cannot show data in recipe-edit

--- src/app/recipe-edit/recipe-edit.ts
+++ src/app/recipe-edit/recipe-edit.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, AsyncPipe } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe/recipe.model';
 import { RecipeService } from '../recipe/recipe.service';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipe-edit',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, AsyncPipe],
   templateUrl: './recipe-edit.html',
   styleUrl: './recipe-edit.scss',
 })
 export class RecipeEdit implements OnInit {  
-  public recipeToEdit: Recipe | undefined;
+  public recipeToEdit$!: Observable<Recipe | undefined>;
   public recipeId: number | undefined;
   public editMode: boolean = false;
   public recipeForm: FormGroup | undefined;
@@ -27,13 +29,12 @@ export class RecipeEdit implements OnInit {
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     if (id !== null && id !== undefined) {
-      this.recipeService.getRecipe(parseInt(id)).subscribe({
-        next: (recipe) => {
+      this.recipeToEdit$ = this.recipeService.getRecipe(parseInt(id)).pipe(
+        tap(recipe => {
           if (recipe) {
-            this.recipeToEdit = recipe;
-            this.editMode = true;
             this.recipeId = recipe.id;
-            this.imageUrl = signal(recipe.imagePath);
+            this.editMode = true;
+            this.imageUrl.set(recipe.imagePath);
             this.recipeForm = new FormGroup({
               'name': new FormControl(recipe.name, Validators.required),
               'description': new FormControl(recipe.description, Validators.required),
@@ -41,25 +42,18 @@ export class RecipeEdit implements OnInit {
               'imagePath': new FormControl(recipe.imagePath, Validators.required)
             });
           }
-        },
-        error: (err) => {
-          console.error('Failed to load recipe', err);
-        }
-      });
+        })
+      );
     }
   }
 
   onSubmit() {
-    // Determine the action based on the mode
     if (this.editMode && this.recipeId != undefined  && this.recipeForm != undefined) {
       this.recipeService.updateRecipe(this.recipeId, this.recipeForm.value);
     }
-    
-    // Navigate back to the recipe list
     this.onCancel();
   }
   
-  // Method to handle canceling the form
   onCancel() {
     this.router.navigate(['../..'], { relativeTo: this.route });
   }
